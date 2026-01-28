5f445042144da25a1b0dc7466cc904872d3243ae
jmohamm1-pixel <jmohamm1@student.gitam.edu>
2026-01-28 10:59:16 +0530

    use_of_pass_continue_break.py

---
+++ 24CSEN1041/use_of_pass_continue_break.py
@@ -0,0 +1,22 @@
+1ST CODE
+for i in range(1, 100):
+    if i % 2 == 0:
+        continue  
+    elif i == 25:
+        break    
+    else:
+        pass     
+    print(i)
+>OUTPUT
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23

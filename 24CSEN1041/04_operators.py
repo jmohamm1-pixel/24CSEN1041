2ca5bcdaba9ffc51e3c7a5df3f330348e5cd07bc
jmohamm1-pixel <jmohamm1@student.gitam.edu>
2026-01-29 15:02:43 +0530

    Create 04_operators.py

---
+++ 24CSEN1041/04_operators.py
@@ -0,0 +1,15 @@
+# Relational Operators
+a = 9
+b = 5
+print("Relational Operators")
+print(f"{a} < {b} = {a < b}")
+print(f"{a} > {b} = {a > b}")
+print(f"{a} == {b} = {a == b}")
+print(f"{a} != {b} = {a != b}\n")
+output
+Relational Operators
+9 < 5 = False
+9 > 5 = True
+9 == 5 = False
+9 != 5 = True
+

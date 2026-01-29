94f45f136c287b3fccd9969ac329fff15a1a8f1b
jmohamm1-pixel <jmohamm1@student.gitam.edu>
2026-01-29 14:59:45 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,17 @@
+a = 9
+b = 5
+
+# Arithmetic Operators
+print("Arithmetic Operators")
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b:.2f}")   # Float division for clarity
+print(f"{a} % {b} = {a % b}\n")
+output
+Arithmetic Operators
+9 + 5 = 14
+9 - 5 = 4
+9 * 5 = 45
+9 / 5 = 1.80
+9 % 5 = 4

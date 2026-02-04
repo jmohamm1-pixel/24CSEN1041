9f0188ac8d8af1f078f09824d0c13c5d179e03d3
jmohamm1-pixel <jmohamm1@student.gitam.edu>
2026-02-04 10:43:37 +0530

    Create string_related_program.py

---
+++ string_related_program.py
@@ -0,0 +1,17 @@
+#Print and format a string
+name = "Python"
+age = 10
+
+print(name + " is " + str(age) + " years old.")
+#output:
+Python is 10 years old.
+
+#code 2
+
+text = "python"
+
+reversed_text = text[::-1]
+print(reversed_text)
+
+#output:
+nohtyp
